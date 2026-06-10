6b88331e9e1a7a8aac1ea702c7a4122cc3ef5c5b
shihab-5 <shihabuli364@gmail.com>
2026-06-10 22:18:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,137 @@
+const express = require('express')
+const app = express()
+const dotenv=require('dotenv')
+const cors=require('cors')
+dotenv.config()
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT|| 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.MONGODB_URI;
+
+// doctime-fMrR3YMWiHvtRr0Y
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+const run= async()=>{
+
+  try {
+
+    const database = client.db("doctime");
+    const userCollection = database.collection("appointments");
+    const bookings = database.collection("bookings");
+    const user=database.collection("user")
+
+
+    app.get('/appointments',async(req,res)=>{
+      const cursor=userCollection.find();
+      const result=await cursor.toArray();
+      res.send(result);
+
+    })
+    app.get('/user',async(req,res)=>{
+      const cursor=user.find();
+      const result=await cursor.toArray();
+      res.send(result);
+
+    })
+
+        app.get('/appointments/:id',async(req,res)=>{
+          const id=req.params.id;
+        console.log(id);
+        const query ={
+          _id:new ObjectId(id)
+        }
+        const user=await userCollection.findOne(query);
+        console.log(id)
+        res.send(user);
+        })
+        
+
+        // app.delete('/users/:id',async(req,res)=>{
+        //   const id =req.params.id;
+        //   const query={
+        //     _id:new ObjectId(id)
+        //   }
+        //   const user=await userCollection.deleteOne(query);
+        //   res.send(user);
+        // })
+        app.patch('/user/:id',async(req,res)=>{
+          const {id}=req.params;
+          const updateData= req.body;
+
+          const result=await user.updateOne(
+            {_id:new ObjectId(id)},
+            {$set:updateData}
+          )
+
+          res.json(result)
+          
+        })
+
+        // app.post('/users',async(req,res)=>{
+        //   const newUser=req.body;
+        //   const result=await userCollection.insertOne(newUser);
+        //   console.log("new user",newUser)
+        //   res.send(result)
+
+        // })
+           app.post('/bookings',async(req,res)=>{
+          const newUser=req.body;
+          const result=await bookings.insertOne(newUser);
+          console.log("new user",newUser)
+          res.send(result)
+
+        })
+        
+        app.get('/bookings/:userId',async(req,res)=>{
+          const {userId}=req.params;
+
+          const result=await bookings.find({ userId :userId}).toArray();
+          res.json(result)
+          
+        })
+        app.delete('/bookings/:bookingId',async(req,res)=>{
+          const {bookingId}=req.params;
+
+          const result=await bookings.deleteOne({ _id :new ObjectId(bookingId)});
+          res.json(result)
+          
+        })
+
+
+    //       app.get('/bookings',async(req,res)=>{
+    //   const cursor=bookings.find();
+    //   const result=await cursor.toArray();
+    //   res.send(result);
+
+    // })
+
+
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+
+}
+
+
+run().catch(console.dir)
