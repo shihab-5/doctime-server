1c245fec3624ae8d0ed852bb894afdc07d05c19c
shihab-5 <shihabuli364@gmail.com>
2026-06-12 16:25:35 +0600

    verify jwt

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors=require('cors')
 dotenv.config()
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet } = require('jose-cjs')
 const port = process.env.PORT|| 5000
 
 app.use(cors())
@@ -29,6 +30,27 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+const JWKS = createRemoteJWKSet(new URL(`http://localhost:3000/api/auth/jwks`));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
 const run= async()=>{
 
   try {
@@ -52,17 +74,7 @@ const run= async()=>{
 
     })
 
-        app.get('/appointments/:id',(req,res,next)=>{
-      const header=req.headers.authorization
-      if(header=='logged in'){
-        next()
-      }
-      else
-      {
-        res.status(401).json({message:"unauthorized"})
-      }
-        }, 
-          async(req,res)=>{
+        app.get('/appointments/:id',verifyToken, async(req,res)=>{
           const id=req.params.id;
         console.log(id);
         const query ={
